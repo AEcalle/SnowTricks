24b5c4c2a1b0661bdae7d6efee866a398f711ead
AEcalle <contact@aurelienecalle.fr>
2021-11-10 16:07:13 +0100

    UserChecker

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User as AppUser;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (! $user instanceof AppUser) {
+            return;
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+        if (! $user instanceof AppUser) {
+            return;
+        }
+
+        if (null === $user->getCreatedAt()) {
+            throw new CustomUserMessageAccountStatusException('Your email is not verified. Please click on the link in the email that has been sent to you.');
+        }
+    }
+}
