4caec72875c4ff771b0cb113fc4b08c8b3b55d02
AEcalle <contact@aurelienecalle.fr>
2021-11-12 10:35:37 +0100

    User Entity - username unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,7 +22,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, unique:true)]
     #[Assert\NotBlank]
     private string $username;
 
